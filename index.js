3afb8dc6ed25078b49b802f2d59fdae3592c1392
brunosm16 <brunoskr23@gmail.com>
2021-09-29 17:52:33 -0300

    MongoDb Connection.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { MONGO_CONNECTION_STR, PORT } = process.env;
 const app = express();
 
+mongoose
+	.connect(MONGO_CONNECTION_STR)
+	.then(console.log('db connection successful'))
+	.catch((err) => {
+		console.log(`something went wrong on db connection : ${err}`);
+	});
+
 // eslint-disable-next-line no-console
-app.listen(3000, () => console.log('server running on port 3000'));
+app.listen(PORT || 3000, () => console.log('server running on port 3000'));
